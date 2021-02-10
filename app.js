6cb2ed3a797eaaf25d44b68558586766be2c64d0
NekoChan <aaron74327@gmail.com>
2021-02-10 13:43:43 -0800

    update

--- app.js
+++ app.js
@@ -1,55 +1,87 @@
+// NekoChanTaiwan Discord.js
+// Discord.js     npm install discord.js        https://discord.js.org/#/docs/main/stable/general/welcome
+// nHentai API    npm install nana-api          https://www.npmjs.com/package/nana-api
+// is-image-url   npm install is-image-url      https://www.npmjs.com/package/is-image-url
+
 'use strict'
 
-// 隨機整數
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+// 初始化變量
 
-// 導入 discord.js module
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-const custom = {
-    // 固定自定狀態 字符串
-    // Activity: '露出中',
+const NanaAPI = require('nana-api')
+const nanaAPI = new NanaAPI
 
-    // 隨機自定狀態（不重複）
+const isImageUrl = require('is-image-url')
+
+// 自定義功能
+const custom = {
+    // 指令前綴
+    commandPrefix: '!',
+
+    // 隨機自定狀態
     randomActivity: {
         // 啟用（布林值）
         Enable: true,
         // 類型（字符串）
         Type: 'COMPETING',
         // 內容（陣列：字符串）
-        Array: ['NekoChan 的肉便器', 'NekoChan 的性奴隸', 'NekoChan 的小母狗', 'NekoChan 的飛機杯'],
+        Array: ['NekoChan 的 肉便器', 'NekoChan 的 性奴隸', 'NekoChan 的 小母狗', 'NekoChan 的 飛機杯'],
         // 切換時間（單位：秒）
-        SwitchTime: 5,
+        SwitchTime: 60,
     },
+
+    // nHentai功能
+    nHentai: {
+        // 隨機本子指令（字符串）
+        randomCommand: 'n',
+        // 隨機本子分類過濾（陣列：字符串）
+        randomTagsFilter: ['futanari', 'scat', 'pig man', 'ryona', 'coprohagia', 'spider girl', 'alien', 'pig girl', 'nipple fuck', 'males only', 'pig', 'yaoi', 'insect', 'guro', 'cannibalism', 'eggs'],
+        // 隨機本子作者過濾（陣列：字符串）
+        randomArtistFilter: ['sakamoto kafka'],
+        // 隨機本字語言過濾（陣列字符串）
+        randomLanguageFilter: ['english', 'japanese'],
+    },
+}
+
+const commandPrefix = new RegExp(custom.commandPrefix)
+
+const S = ' [系統] ', E = ' [事件] '
+
+let embed = null, artistName = '', artistUrl = '', tagsName = '', coverUrl = '', language = ''
+
+console.log(`[${getTime()}]${S}腳本讀取中`)
+
+// 隨機整數
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+// 當前時間
+function getTime() {
+    return new Date().toLocaleString()
+}
+// nHentai 過濾
+function nHentaiFilter(array, name) {
+    if (array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            if (name === array[i]) {
+                console.log(`[${getTime()}]${E} 發現過濾類型：${name}`)
+                return true
+            }
+        }
+    }
 }
 
 
 // 事件綁定
 client.on('ready', () => {
-    // 初始化變量
-    const E = '[事件] '
-    let randomNum = null, currentNum = null
-    // https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-ready
-    console.log(`${E}${client.user.tag} 已登入 關閉請按 "Ctrl + C"`)
-
-    // 狀態
-    // https://discord.js.org/#/docs/main/stable/class/ClientUser?scrollTo=setActivity
+    console.log(`[${getTime()}]${E}已登入 ${client.user.tag}`)
 
     // 隨機自定狀態
     const SetRandomActivity = () => {
-        randomNum = getRandomInt(custom.randomActivity.Array.length)
-        currentNum = randomNum != currentNum ? randomNum : false
-        if (currentNum === false) {
-            return SetRandomActivity()
-        }
-        client.user.setActivity(custom.randomActivity.Array[currentNum], {type: custom.randomActivity.Type})
-            .then(presence => {
-                console.log(`${E}已設置狀態：${presence.activities[0].name}[${currentNum}]`)
-                currentNum = randomNum
-            })
+        client.user.setActivity(custom.randomActivity.Array[getRandomInt(custom.randomActivity.Array.length)], {type: custom.randomActivity.Type})
+            .then(presence => console.log(`[${getTime()}]${E}已設置狀態：${presence.activities[0].name}`))
             .catch(error => console.log(error))
         setTimeout(() => {
             SetRandomActivity()
@@ -58,6 +90,96 @@ client.on('ready', () => {
     custom.randomActivity.Enable ? SetRandomActivity() : null
 })
 
+client.on('message', message => {
+    // 指令前綴偵測
+    if (commandPrefix.test(message.content)) {
+        // 指令偵測
+        switch (message.content) {
+            // nHentai 隨機本子
+            case `${custom.commandPrefix}${custom.nHentai.randomCommand}`:
+                const nHentaiRandom = () => {
+                    // nHentai API .random()
+                    nanaAPI.random()
+                        .then(book => {
+                            // type 處理
+                            for (let value of book.tags) {
+                                if (value.type === 'tag') {
+                                    // 過濾分類
+                                    if (nHentaiFilter(custom.nHentai.randomTagsFilter, value.name) === true) {
+                                        return nHentaiRandom()
+                                    }
+                                    tagsName += `${value.name}, `
+                                } else {
+                                    switch (value.type) {
+                                            case 'artist':
+                                                // 過濾作者
+                                                if (nHentaiFilter(custom.nHentai.randomArtistFilter, value.name) === true) {
+                                                    return nHentaiRandom()
+                                                }
+                                                artistName = value.name
+                                                artistUrl = `https://nhentai.net${value.url}`
+                                                break
+                                            case 'language':
+                                                if (value.name === 'english' ||
+                                                    value.name === 'japanese' ||
+                                                    value.name === 'chinese') {
+                                                         // 過濾語言
+                                                        if (nHentaiFilter(custom.nHentai.randomLanguageFilter, value.name) === true) {
+                                                            return nHentaiRandom()
+                                                        }
+                                                        language = value.name
+                                                    }
+                                                break
+                                    }
+                                }
+                            }
+
+                            // 空值檢查
+                            artistName = artistName === '' ? '未分類' : artistName
+                            artistUrl = artistUrl === '' ? 'https://nhentai.net/' : artistUrl
+                            tagsName = tagsName === '' ? '未分類' : tagsName
+
+                            // 圖片檢查
+                            if (isImageUrl(`https://t.nhentai.net/galleries/${book.media_id}/cover.jpg`)) {
+                                coverUrl = `https://t.nhentai.net/galleries/${book.media_id}/cover.jpg`
+                            } else if (isImageUrl(`https://t.nhentai.net/galleries/${book.media_id}/cover.png`)) {
+                                coverUrl = `https://t.nhentai.net/galleries/${book.media_id}/cover.png`
+                            }
+
+                            // Embed
+                            embed = new Discord.MessageEmbed()
+                                .setColor('#ed2553')
+                                .setTitle(book.title.pretty)
+                                .setURL(`https://nhentai.net/g/${book.id}/`)
+                                .setAuthor(`作者：${artistName}`, '', artistUrl)
+                                .setThumbnail('https://i.imgur.com/r36VxQt.png')
+                                // .setDescription('隨機本子')
+                                .addFields(
+                                    { name: 'ＩＤ：', value: book.id, inline: true},
+                                    { name: '語言：', value: language, inline: true },
+                                    { name: '分類：', value: tagsName },
+                                )
+                                .setImage(coverUrl)
+                                .setFooter(`頁數：${book.num_pages}`)
+
+                            // 發送訊息
+                            message.channel.send(embed)
+                                .then(() => console.log(`[${getTime()}]${E}已發送本子：${book.id}`))
+                                .catch(error => console.log(error))
+
+                            // 重置變量
+                            artistName = ''
+                            artistUrl = ''
+                            tagsName = ''
+                        })
+                }
+                nHentaiRandom()
+                break
+        }
+    }
+})
+
+console.log(`[${getTime()}]${S}腳本讀取完成`)
+
 // 登入
-// https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=login
 client.login('')
